52ac6e9ad2411ca9d2254cd552e4961065260a66
Shashika Samaradeera <thushanishashika23@gmail.com>
2023-11-17 16:18:45 +0100

    Adding Surrener and Healing functions

--- app.js
+++ app.js
@@ -7,20 +7,45 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
+      winner: null,
     };
+  },
+
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winer = "player";
+      }
+    },
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) return { width: "0%" };
       return {
         width: this.monsterHealth + "%",
       };
     },
     playerBarStyle() {
+      if (this.playerHealth < 0) return { width: "0%" };
       return { width: this.playerHealth + "%" };
+    },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
     },
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
@@ -29,6 +54,32 @@ const app = Vue.createApp({
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
+    },
+    startGame() {
+      (this.playerHealth = 100),
+        (this.monsterHealth = 100),
+        (this.currentRound = 0),
+        (this.winner = null);
+    },
+    surrender() {
+      this.winner = "monster";
     },
   },
 });
